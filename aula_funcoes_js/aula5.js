9b2791f6fa582e72df2fb112359de28823d30aeb
Rafaela <oliveirarafaela865@gmail.com>
2022-09-27 10:52:49 -0300

    Adicionando aula 5, atividade 1 e 2

---
+++ aula_funcoes_js/aula5.js
@@ -0,0 +1,19 @@
+// Arrow functions
+// sem arrow functions
+
+    const helloWorld = function() {
+        return "hello World";
+    }
+    console.log(helloWorld())
+
+//Com Arro Functions = a sintaxe dela e o operador (=>)
+
+    const helloWorld2 = () => {
+        return "Hello World!";
+    }
+    console.log(helloWorld2())
+
+//ou
+
+    const helloWorld3 = () => "Hello World!!"
+    console.log(helloWorld3())
