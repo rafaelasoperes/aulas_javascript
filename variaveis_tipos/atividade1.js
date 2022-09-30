54a0a7d1ba65e8246896dc745c4a2c9180a75666
Rafaela <oliveirarafaela865@gmail.com>
2022-09-30 15:25:21 -0300

    pasta da aula de Váriaveis e Tipos

---
+++ variaveis_tipos/atividade1.js
@@ -0,0 +1,22 @@
+// solução 1
+    // function verificaPalindromo(string) {
+    //     if (!string) return "string inexistente";
+
+    //     return string.split("").reverse().join("") === string;
+    // }
+    // console.log(verificaPalindromo("ovo"))
+
+// solução 2
+
+function verificaPalindromo2(string) {
+    if (!string) return "String Inexistente";
+
+    for (let i = 0; i < string.length / 2; i++) {
+        if(string[i] !== string[string.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(verificaPalindromo2("ovo"))
