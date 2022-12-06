6bdf5657af04692a011477c232096ad1536481cf
Rafaela <oliveirarafaela865@gmail.com>
2022-12-06 20:34:54 -0300

    retomando as aulas

--- alura_javascript/orientacao_objeto/index.js
+++ alura_javascript/orientacao_objeto/index.js
@@ -6,14 +6,25 @@ class Cliente {
 
 class ContaCorrente {
 	agencia;
-	saldo;
+	_saldo = 0;
+
+	sacar(valor) {
+		if(this._saldo >= valor) {
+			this ._saldo -= valor;
+			return valor;
+		}
+	}
+
+	depositar(valor) {
+		if(valor <= 0) {
+			return;
+		}
+		this._saldo += valor;
+	}
 }
-
-
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
@@ -21,12 +32,14 @@ cliente2.cpf = 88822233309;
 
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
 
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
 
-console.log(cliente1);
-console.log(cliente2);
+valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
+
