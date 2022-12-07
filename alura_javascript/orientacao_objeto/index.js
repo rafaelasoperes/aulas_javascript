cc9b851a6b0cef6cf084f09b71e15667af183593
Rafaela <oliveirarafaela865@gmail.com>
2022-12-07 07:42:39 -0300

    adicionando o arquivo Package.json

--- alura_javascript/orientacao_objeto/index.js
+++ alura_javascript/orientacao_objeto/index.js
@@ -1,27 +1,6 @@
-class Cliente {
-	nome;
-	cpf;
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 
-}
-
-class ContaCorrente {
-	agencia;
-	_saldo = 0;
-
-	sacar(valor) {
-		if(this._saldo >= valor) {
-			this ._saldo -= valor;
-			return valor;
-		}
-	}
-
-	depositar(valor) {
-		if(valor <= 0) {
-			return;
-		}
-		this._saldo += valor;
-	}
-}
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
@@ -38,7 +17,7 @@ contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.depositar(100);
 
-valorSacado = contaCorrenteRicardo.sacar(50);
+const valorSacado = contaCorrenteRicardo.sacar(50);
 console.log(valorSacado);
 
 console.log(contaCorrenteRicardo);
