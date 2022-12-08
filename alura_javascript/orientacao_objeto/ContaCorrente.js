780a84c74a3308529a4f8e76b7045bd103612dd5
Rafaela <oliveirarafaela865@gmail.com>
2022-12-07 21:56:39 -0300

    aula 07/12

--- alura_javascript/orientacao_objeto/ContaCorrente.js
+++ alura_javascript/orientacao_objeto/ContaCorrente.js
@@ -1,5 +1,7 @@
 export class ContaCorrente {
 	agencia;
+    cliente;
+
 	_saldo = 0;
 
 	sacar(valor) {
@@ -15,4 +17,9 @@ export class ContaCorrente {
 		}
 		this._saldo += valor;
 	}
+
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
 }
