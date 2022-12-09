a773c7afb578af171313b8f4355deaf6a8e0d462
Rafaela <oliveirarafaela865@gmail.com>
2022-12-09 17:30:23 -0300

    Finalizando o curso de orientação objeto da Alura

--- alura_javascript/orientacao_objeto/ContaCorrente.js
+++ alura_javascript/orientacao_objeto/ContaCorrente.js
@@ -1,8 +1,30 @@
-export class ContaCorrente {
-	agencia;
-    cliente;
+import { Cliente } from "./Cliente.js";
 
-	_saldo = 0;
+export class ContaCorrente {
+    static numeroDeContas = 0;
+	agencia;
+    _cliente;
+    _saldo = 0;
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente () {
+        return this._cliente
+    }
+
+    get saldo () {
+        return this._saldo
+    }
+
+    constructor(agencia, cliente) {
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas++;
+    }
 
 	sacar(valor) {
 		if(this._saldo >= valor) {
