aa9c3ce591d9643c60b0de875150e61b7fcdfad0
Rafaela <rafaelasoperes@gmail.com>
2023-01-12 20:43:16 -0300

    Finalizando o arquivo de autenticação

--- alura_javascript/orientacao_objeto/index.js
+++ alura_javascript/orientacao_objeto/index.js
@@ -1,14 +1,18 @@
 import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./ContaCorrente.js";
+import {Gerente} from "./Funcionario/Gerente.js"
+import {Diretor} from "./Funcionario/Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
-const cliente1 = new Cliente("Ricardo",11122233309);
-const cliente2 = new Cliente("Alice", 88822233309);
+const diretor =  new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456")
+const gerente =  new Gerente("Ricardo",  5000, 12378945601);
+gerente.cadastrarSenha("123");
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
-contaCorrenteRicardo.depositar(500);
+const cliente = new Cliente("Lais", 78945612379, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
 
-const conta2 = new ContaCorrente(102, cliente2);
 
-contaCorrenteRicardo.transferir(200, conta2)
-console.log(conta2)
-console.log(ContaCorrente.numeroDeContas)
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
